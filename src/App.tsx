41af102b7f3a5556c137ea26400ed134f89fe161
odajima-ca <odajima_yu_gn@cyberagent.co.jp>
2023-01-23 10:55:15 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import "./styles.css";
+import React from 'react';
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
+
+export default App;
