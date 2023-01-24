55eb2d29b9df50702adbae1ce34736b7d6a74b95
odajima-ca <odajima_yu_gn@cyberagent.co.jp>
2023-01-24 17:19:55 +0900

    fix

--- src/core/AppRoutes.tsx
+++ src/core/AppRoutes.tsx
@@ -2,12 +2,14 @@ import React, { FC } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import { AppLayout } from "../layouts/AppLayout";
+import { CameraPage } from "../pages/CameraPage";
 import { TopPage } from "../pages/TopPage";
 
 export const AppRoutes: FC = () => (
   <Routes>
     <Route element={<AppLayout />}>
       <Route element={<TopPage />} path="/" />
+      <Route element={<CameraPage />} path="/camera" />
     </Route>
   </Routes>
 );
--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,27 +1,60 @@
-import AppBar from "@mui/material/AppBar";
+import CameraAltIcon from "@mui/icons-material/CameraAlt";
+import HomeIcon from "@mui/icons-material/Home";
+import { SpeedDialAction, useTheme } from "@mui/material";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Toolbar from "@mui/material/Toolbar";
+import SpeedDial from "@mui/material/SpeedDial";
+import SpeedDialIcon from "@mui/material/SpeedDialIcon";
 import { FC } from "react";
-import { useOutlet } from "react-router-dom";
+import { use100vh } from "react-div-100vh";
+import { Link, useOutlet } from "react-router-dom";
 
 export const AppLayout: FC = () => {
+  const theme = useTheme();
   const outlet = useOutlet();
+  const height = use100vh();
 
   return (
-    <Box sx={{ display: "flex", width: "100vw" }}>
-      <AppBar component="nav">
-        <Toolbar sx={{ gridGap: "4px" }}>
-          <Button color="inherit" fullWidth={true} href="/">
-            Top
-          </Button>
-        </Toolbar>
-      </AppBar>
+    <Box sx={{ height, width: "100vw" }}>
+      {outlet}
 
-      <Box component="main" sx={{ p: 3, width: "100%" }}>
-        <Toolbar />
-        {outlet}
-      </Box>
+      <SpeedDial
+        ariaLabel="SpeedDial basic example"
+        icon={<SpeedDialIcon />}
+        open={true}
+        sx={{
+          "& svg": {
+            display: "block",
+          },
+          "&.MuiSpeedDial-directionDown, &.MuiSpeedDial-directionRight": {
+            left: theme.spacing(2),
+            top: theme.spacing(2),
+          },
+          "&.MuiSpeedDial-directionUp, &.MuiSpeedDial-directionLeft": {
+            bottom: theme.spacing(2),
+            right: theme.spacing(2),
+          },
+          bottom: 16,
+          position: "absolute",
+          right: 16,
+        }}
+      >
+        <SpeedDialAction
+          icon={
+            <Link to="/">
+              <HomeIcon />
+            </Link>
+          }
+          tooltipTitle="ホーム"
+        />
+        <SpeedDialAction
+          icon={
+            <Link to="/camera">
+              <CameraAltIcon />
+            </Link>
+          }
+          tooltipTitle="カメラ"
+        />
+      </SpeedDial>
     </Box>
   );
 };
