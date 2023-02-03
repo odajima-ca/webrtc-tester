415372414ec1cd9eb4e81925251f7895cff350b0
odajima-ca <odajima_yu_gn@cyberagent.co.jp>
2023-02-03 09:30:27 +0900

    fix

--- src/pages/QrReaderPage.tsx
+++ src/pages/QrReaderPage.tsx
@@ -8,8 +8,6 @@ import { useMediaStream, useStopMediaStream } from "../providers/MediaStreamProv
 export const QrReaderPage: FC = () => {
   const [codeReader, setCodeReader] = useState<BrowserQRCodeReader | undefined>();
 
-  const [resultText, setResultText] = useState("");
-
   const { videoRef, canvasRef, isVideoPlayed } = useMediaStream();
   useStopMediaStream();
 
@@ -19,66 +17,55 @@ export const QrReaderPage: FC = () => {
   }, []);
 
   useEffect(() => {
-    if (!isVideoPlayed) return;
-    if (!codeReader) return;
+    const scan = async () => {
+      if (!isVideoPlayed) return;
+      if (!codeReader) return;
 
-    const video = videoRef.current;
+      const canvas = canvasRef.current;
+      const video = videoRef.current;
 
-    if (!video) return;
+      if (!canvas || !video) return;
 
-    codeReader.decodeFromVideoElement(video as HTMLVideoElement, (result) => {
-      if (result) {
-        setResultText(result.getText());
+      const context = canvas.getContext("2d");
+
+      if (!context) return;
+
+      const height = video.videoHeight;
+      const width = video.videoWidth;
+
+      canvas.height = height;
+      canvas.width = width;
+
+      console.debug("QR Decode");
+      context.drawImage(video, 0, 0, width, height);
+
+      try {
+        const result = await codeReader.decodeFromCanvas(canvas);
+
+        if (result) {
+          const text = result.getText();
+          console.info("QR Decoded", text);
+        }
+      } catch (error) {
+        // noop
       }
-    });
-  }, [codeReader, isVideoPlayed, videoRef]);
 
-  // useEffect(() => {
-  //   const scan = async () => {
-  //     if (!isVideoPlayed) return;
-  //     if (!codeReader) return;
-  //
-  //     const canvas = canvasRef.current;
-  //     const video = videoRef.current;
-  //
-  //     if (!canvas || !video) return;
-  //
-  //     const context = canvas.getContext("2d");
-  //
-  //     if (!context) return;
-  //
-  //     // console.log("scan");
-  //
-  //     const height = video.videoHeight;
-  //     const width = video.videoWidth;
-  //
-  //     canvas.height = height;
-  //     canvas.width = width;
-  //
-  //     // context.drawImage(video, 0, 0, width, height);
-  //     await codeReader.decodeFromVideoElement(video as HTMLVideoElement, (result) => {
-  //       if (result) {
-  //         setResultText(result.getText());
-  //       }
-  //     });
-  //
-  //     requestAnimationFrame(scan);
-  //   };
-  //
-  //   const timerId = requestAnimationFrame(scan);
-  //
-  //   return () => {
-  //     cancelAnimationFrame(timerId);
-  //   };
-  // }, [canvasRef, codeReader, isVideoPlayed, videoRef]);
+      requestAnimationFrame(scan);
+    };
+
+    const timerId = requestAnimationFrame(scan);
+
+    return () => {
+      cancelAnimationFrame(timerId);
+    };
+  }, [canvasRef, codeReader, isVideoPlayed, videoRef]);
 
   return (
     <AppLayout>
       <Box sx={{ "&>video": { height: "100%", width: "100%" }, height: "100%", width: "100%" }}>
         <video height="100%" ref={videoRef} width="100%" />
+        <canvas ref={canvasRef} style={{ display: "none" }} />
       </Box>
-
-      {resultText}
     </AppLayout>
   );
 };
