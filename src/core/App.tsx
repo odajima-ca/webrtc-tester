ef4e2917344c9c16656126a241f8ef56b1f00d8a
odajima-ca <odajima_yu_gn@cyberagent.co.jp>
2023-02-03 09:21:02 +0900

    fix

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,13 +1,33 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
+import { use100vh } from "react-div-100vh";
+import VConsole from "vconsole";
 
 import { MediaStreamProvider } from "../providers/MediaStreamProvider";
 import { SnackbarProvider } from "../providers/SnackbarProvider";
 import { AppRoutes } from "./AppRoutes";
 
-export const App: FC = () => (
-  <SnackbarProvider>
-    <MediaStreamProvider>
-      <AppRoutes />
-    </MediaStreamProvider>
-  </SnackbarProvider>
-);
+export const App: FC = () => {
+  const height = use100vh();
+
+  useEffect(() => {
+    const vConsole = new VConsole({
+      onReady: () => {
+        if (height) {
+          vConsole.setSwitchPosition(16, height - 52);
+        }
+      },
+    });
+
+    return () => {
+      vConsole.destroy();
+    };
+  }, [height]);
+
+  return (
+    <SnackbarProvider>
+      <MediaStreamProvider>
+        <AppRoutes />
+      </MediaStreamProvider>
+    </SnackbarProvider>
+  );
+};
