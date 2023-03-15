1cb0264aa97f228a7c9cd17e98c94eba2b53739b
odajima-ca <odajima_yu_gn@cyberagent.co.jp>
2023-03-15 10:57:10 +0900

    ix

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -7,67 +7,67 @@ export type VideoProps = React.VideoHTMLAttributes<HTMLVideoElement>;
 export const Video = React.forwardRef<VideoRef, VideoProps>((props, ref) => (
   <video
     onAbort={(event) => {
-      console.log("onAbort", event);
+      // console.log("onAbort", event);
     }}
     onCanPlay={(event) => {
-      console.log("onCanPlay", event);
+      // console.log("onCanPlay", event);
     }}
     onCanPlayThrough={(event) => {
-      console.log("onCanPlayThrough", event);
+      // console.log("onCanPlayThrough", event);
     }}
     onDurationChange={(event) => {
-      console.log("onDurationChange", event);
+      // console.log("onDurationChange", event);
     }}
     onEmptied={(event) => {
-      console.log("onEmptied", event);
+      // console.log("onEmptied", event);
     }}
     onEnded={(event) => {
-      console.log("onEnded", event);
+      // console.log("onEnded", event);
     }}
     onError={(event) => {
-      console.log("onError", event);
+      // console.log("onError", event);
     }}
     onLoadedData={(event) => {
-      console.log("onLoadedData", event);
+      // console.log("onLoadedData", event);
     }}
     onLoadedMetadata={(event) => {
-      console.log("onLoadedMetadata", event);
+      // console.log("onLoadedMetadata", event);
     }}
     onPause={(event) => {
-      console.log("onPause", event);
+      // console.log("onPause", event);
     }}
     onPlay={(event) => {
-      console.log("onPlay", event);
+      // console.log("onPlay", event);
     }}
     onPlaying={(event) => {
-      console.log("onPlaying", event);
+      // console.log("onPlaying", event);
     }}
     onProgress={(event) => {
-      console.log("onProgress", event);
+      // console.log("onProgress", event);
     }}
     onRateChange={(event) => {
-      console.log("onRateChange", event);
+      // console.log("onRateChange", event);
     }}
     onSeeked={(event) => {
-      console.log("onSeeked", event);
+      // console.log("onSeeked", event);
     }}
     onSeeking={(event) => {
-      console.log("onSeeking", event);
+      // console.log("onSeeking", event);
     }}
     onStalled={(event) => {
-      console.log("onStalled", event);
+      // console.log("onStalled", event);
     }}
     onSuspend={(event) => {
-      console.log("onSuspend", event);
+      // console.log("onSuspend", event);
     }}
     onTimeUpdate={(event) => {
-      console.log("onTimeUpdate", event);
+      // console.log("onTimeUpdate", event);
     }}
     onVolumeChange={(event) => {
-      console.log("onVolumeChange", event);
+      // console.log("onVolumeChange", event);
     }}
     onWaiting={(event) => {
-      console.log("onWaiting", event);
+      // console.log("onWaiting", event);
     }}
     ref={ref}
     {...props}
